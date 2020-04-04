fe55380284d17901f491055c02b223cf10c9d249
Parth Samnani <samnaniparth@gmail.com>
2020-04-04 17:30:13 +0530

    commented location and dropdown

--- shopstock/lib/shopkeeperSignUp.dart
+++ shopstock/lib/shopkeeperSignUp.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-class shopKeeperSignIn extends StatelessWidget {
+class shopKeeperSignIn extends StatefulWidget {
+  @override
+  _shopKeeperSignInState createState() => _shopKeeperSignInState();
+}
+
+class _shopKeeperSignInState extends State<shopKeeperSignIn> {
   Widget _buildName() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -56,6 +61,67 @@ class shopKeeperSignIn extends StatelessWidget {
       ],
     );
   }
+
+  Widget _buildMobileNo() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          "Phone Number",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Container(
+          child: TextField(
+            keyboardType: TextInputType.phone,
+            style: TextStyle(color: Colors.black),
+            decoration: InputDecoration(
+                contentPadding: EdgeInsets.only(top: 10),
+                prefixIcon: Icon(
+                  Icons.phone,
+                  color: Colors.black,
+                ),
+                hintText: 'Mobile Number'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildGSTID() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          "GST ID",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Container(
+          child: TextField(
+            keyboardType: TextInputType.phone,
+            style: TextStyle(color: Colors.black),
+            decoration: InputDecoration(
+                contentPadding: EdgeInsets.only(top: 10),
+                prefixIcon: Icon(
+                  Icons.apps,
+                  color: Colors.black,
+                ),
+                hintText: 'GST ID'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  List<String> _shoptype = ["Grocery", "Medical"];
+
+  String _selectedshoptype;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -101,10 +167,72 @@ class shopKeeperSignIn extends StatelessWidget {
                 Column(
                   children: <Widget>[
                     _buildName(),
-                    SizedBox(height: 50,),
+                    SizedBox(
+                      height: 20,
+                    ),
                     _buildShopName(),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    _buildMobileNo(),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    _buildGSTID(),
                   ],
                 ),
+                SizedBox(
+                  height: 50,
+                ),
+//                Container(
+//                  height: 50,
+//                  width: 200,
+//                  child: RaisedButton.icon(
+//                    shape: RoundedRectangleBorder(
+//                        borderRadius: BorderRadius.circular(10)),
+//                    color: Theme.of(context).primaryColor,
+//                    onPressed: () {},
+//                    icon: Icon(
+//                      Icons.location_on,
+//                      color: Colors.white,
+//                    ),
+//                    label: Text(
+//                      "Use Current Location",
+//                      style: TextStyle(color: Colors.white),
+//                    ),
+//                  ),
+//                ),
+//                SizedBox(
+//                  height: 30,
+//                ),
+//                Row(
+//                  mainAxisAlignment: MainAxisAlignment.start,
+//                  children: <Widget>[
+//                    Column(
+//                      children: <Widget>[
+//                        Text("Shop type"),
+//                        SizedBox(
+//                          height: 20,
+//                        ),
+//                        DropdownButton(
+//                          items: _shoptype.map((shoptype) {
+//                            return DropdownMenuItem(
+//                              child: new Text(shoptype),
+//                              value: shoptype,
+//                            );
+//                          }).toList(),
+//                          onChanged: (newValue) {
+//                            setState(() {
+//                              _selectedshoptype = newValue;
+//                            });
+//                          },
+//                          hint: Text("Please choose your college"),
+//                          value: _selectedshoptype,
+//                        ),
+//                      ],
+//                    )
+//                  ],
+//                ),
               ],
             ),
           ),
