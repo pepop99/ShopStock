f64b91e7ed1f38dee940ab782b992dea0c1dfdb8
Parth Samnani <samnaniparth@gmail.com>
2020-04-04 13:00:24 +0530

    made page scrollable

--- shopstock/lib/signin.dart
+++ shopstock/lib/signin.dart
@@ -9,17 +9,17 @@ class signIn extends StatefulWidget {
 class _signInState extends State<signIn> {
   bool english = false;
 
-  void _onEnglish(bool newValue) => setState((){
-    english = newValue;
-  });
+  void _onEnglish(bool newValue) => setState(() {
+        english = newValue;
+      });
 
 //  List<String> languages = ["English", "हिन्दी","मराठी","తెలుగు","മലയാളം"];
   Map<String, bool> langs = {
-  "English" : true,
-  "हिन्दी" : false,
-  "मराठी" : false,
-  "తెలుగు" : false,
-  "മലയാളം" : false,
+    "English": true,
+    "हिन्दी": false,
+    "मराठी": false,
+    "తెలుగు": false,
+    "മലയാളം": false,
   };
   bool _isChecked = true;
   String _currLang = "English";
@@ -29,81 +29,95 @@ class _signInState extends State<signIn> {
     return Container(
       height: double.infinity,
       color: Theme.of(context).primaryColor,
-      child: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(top: 100),
-            child: Center(
-              child: Icon(
-                Icons.shopping_cart,
-                color: Colors.white,
-                size: 130,
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(top: 100),
+              child: Center(
+                child: Icon(
+                  Icons.shopping_cart,
+                  color: Colors.white,
+                  size: 130,
+                ),
               ),
             ),
-          ),
-          Text(
-            "ShopStock",
-            style: TextStyle(
-                color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 40),
-          Text(
-            "Please Select your Preferred Language",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
+            Text(
+              "ShopStock",
+              style: TextStyle(
+                  color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
             ),
-            textAlign: TextAlign.center,
-          ),
-          Card(
-            color: Colors.white,
-            child: Column(children:
+            SizedBox(height: 40),
+            Text(
+              "Please Select your Preferred Language",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Container(
+              height: 500,
+              child: Card(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                color: Colors.white,
+                child: Column(
+                  children: <Widget>[
+                    Column(
+                      children:
 //              Row(children: <Widget>[
 //                new Checkbox(value: english, onChanged: _onEnglish),
 //
 //              ],),
-              langs.keys.map((String t) => CheckboxListTile(
-                title: new Text(t),
-                value: langs[t],
-                onChanged: (bool val) {
-                  setState(() {
-                    langs[t] = val;
-                  });
-                  langs[_currLang] = false;
-                  _currLang = t;
-                },
-              )).toList()
-            ),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          RaisedButton.icon(
-              color: Theme.of(context).primaryColor,
-              onPressed: () {},
-              icon: Icon(
-                Icons.location_on,
-                color: Colors.white,
-              ),
-              label: Text(
-                "Use your current location",
-                style: TextStyle(color: Colors.white),
-              )),
-          SizedBox(
-            height: 33,
-          ),
-          RaisedButton(
-            child: Text(
-              "NEXT",
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
+                          langs.keys
+                              .map((String t) => CheckboxListTile(
+                                    title: new Text(t),
+                                    value: langs[t],
+                                    onChanged: (bool val) {
+                                      setState(() {
+                                        langs[t] = val;
+                                      });
+                                      langs[_currLang] = false;
+                                      _currLang = t;
+                                    },
+                                  ))
+                              .toList(),
+                    ),
+                    SizedBox(
+                      height: 100,
+                    ),
+                    RaisedButton(
+                      child: Text(
+                        "NEXT",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () {},
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ],
+                ),
               ),
             ),
-            onPressed: () {},
-            color: Theme.of(context).primaryColor,
-          ),
-        ],
+
+//          RaisedButton.icon(
+//              color: Theme.of(context).primaryColor,
+//              onPressed: () {},
+//              icon: Icon(
+//                Icons.location_on,
+//                color: Colors.white,
+//              ),
+//              label: Text(
+//                "Use your current location",
+//                style: TextStyle(color: Colors.white),
+//              )),
+//          SizedBox(
+//            height: 33,
+//          ),
+          ],
+        ),
       ),
     );
   }
